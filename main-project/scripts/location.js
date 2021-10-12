9029d7b8330ad4f1b1ce699be9f13d19bf89eebc
Damian <larson98@gmail.com>
2021-10-12 23:26:45 +0200

    Fixed mobile and big screen issues

--- main-project/scripts/location.js
+++ main-project/scripts/location.js
@@ -4,6 +4,7 @@ let lat = 51.50853;
 let lng = -0.12574;
 
 var mymap = L.map('mapid').setView([lat, lng], 13);
+mymap.removeControl(mymap.zoomControl);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
